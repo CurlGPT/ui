11027ffe41a15a4f8f4b94fcf9ef9a94292b998d
Lokeswaran Aruljothi <11lokeswaran@gmail.com>
2024-02-17 21:42:07 +0530

    feat:add button variants

--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,12 +1,21 @@
-import { FC } from "react";
+import { ButtonHTMLAttributes, FC } from "react";
 
-interface ButtonProps {
-    label: string;
+export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+    variant?: "primary" | "secondary";
 }
 
 const Button: FC<ButtonProps> = (props) => {
-    const { label } = props;
-    return <div className="p-2 b-2">{label}</div>;
+    const { variant, children } = props;
+    const colorClass = variant === "primary" ? "blue" : "red";
+    return (
+        <button
+            style={{ backgroundColor: colorClass }}
+            {...props}
+            className="p-2 b-2"
+        >
+            {children}
+        </button>
+    );
 };
 
 export default Button;
